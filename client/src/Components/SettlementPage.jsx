7b6c5784dcb46705a85ff726804a3be4ac482375
kiran-c8 <kiranc8298@gmail.com>
2022-08-04 12:35:48 +0530

    settlement bug fix

--- client/src/Components/SettlementPage.jsx
+++ client/src/Components/SettlementPage.jsx
@@ -7,18 +7,22 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Image from "../assets/72991-payer.gif";
 import { baseUrl } from "./Constants";
+import Loader from "./Loader";
 const SettlementPage = () => {
   const groupId = new URLSearchParams(useLocation().search).get("groupId");
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
   const token = localStorage.getItem("authToken");
   useEffect(() => {
+    showLoader();
     axios
       .get(`${baseUrl}/expense/settle?groupId=${groupId}`, {
         headers: { authorization: `Bearer ${token}` },
       })
       .then((response) => {
         setData(response.data);
+        hideLoader();
       })
       .catch((err) => {
         if (err.response.status === 403 || err.response.status === 401) {
@@ -26,50 +30,63 @@ const SettlementPage = () => {
         }
       });
   }, []);
+
+  const showLoader = () => {
+    setLoader(true);
+  };
+  const hideLoader = () => {
+    setLoader(false);
+  };
   return (
     <div>
-      <Container
-        sx={{
-          display: "flex",
-          minHeight: "80vh",
-          flexDirection: { md: "row", xs: "column" },
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Box
-          sx={{
-            width: { md: "50%", xs: "100%" },
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <img src={Image} alt="" />
-        </Box>
-        <Box
-          sx={{
-            width: { md: "50%", xs: "100%" },
-            marginTop: { md: "0", xs: "50px" },
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {data.map((item) => {
-            return (
-              <Typography
-                sx={{
-                  fontSize: { md: "25px", xs: "18px" },
-                  textAlign: "center",
-                }}
-              >
-                {item}
-              </Typography>
-            );
-          })}
-        </Box>
-      </Container>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Container
+            sx={{
+              display: "flex",
+              minHeight: "80vh",
+              flexDirection: { md: "row", xs: "column" },
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                width: { md: "50%", xs: "100%" },
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <img src={Image} alt="" />
+            </Box>
+            <Box
+              sx={{
+                width: { md: "50%", xs: "100%" },
+                marginTop: { md: "0", xs: "50px" },
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {data.map((item) => {
+                return (
+                  <Typography
+                    sx={{
+                      fontSize: { md: "25px", xs: "18px" },
+                      textAlign: "center",
+                    }}
+                  >
+                    {item}
+                  </Typography>
+                );
+              })}
+            </Box>
+          </Container>
+        </>
+      )}
     </div>
   );
 };
