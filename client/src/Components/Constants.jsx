63f5f45c30152bc3e4eab6014631156fe28f8b0f
kiran-c8 <kiranc8298@gmail.com>
2023-08-28 13:23:03 +0530

    deployment changes

--- client/src/Components/Constants.jsx
+++ client/src/Components/Constants.jsx
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://44.205.243.151:8080';
+export const baseUrl = 'http://localhost:8080';
--- server/app.js
+++ server/app.js
@@ -15,6 +15,24 @@ app.use(cors());
 app.use("/user", userRouter);
 app.use("/expense", expenseRouter);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 app.get("/",(req,res)=>{
     res.send("API is running")
 })
